2807a5523b2e34b59b82777cd3aa83bcef0bc094
Agustín Murseli <agustinmurseli@gmail.com>
2026-07-21 22:40:30 -0300

    03 - lección

--- chapter_1/03/exercise/script.js
+++ chapter_1/03/exercise/script.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three';
 
+//canvas
+const canvas = document.querySelector('canvas.webgl');
+console.log(canvas);
+
 //escena
 const scene = new THREE.Scene();
 
@@ -9,4 +13,21 @@ const material = new THREE.MeshBasicMaterial({ color: 'red' });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+
+//tamaños
+const sizes = {
+    width: 800,
+    height: 600
+}
+
 //camara
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
+camera.position.z = 3;
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
