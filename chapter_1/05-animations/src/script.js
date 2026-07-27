4e22bcd51589c95aae9b860998efb4c877088c9b
Agustín Murseli <agustinmurseli@gmail.com>
2026-07-26 21:06:11 -0300

    Lesson 6

---
+++ chapter_1/05-animations/src/script.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three'
+import { Timer } from 'three/addons/misc/Timer.js'
+import gsap from 'gsap'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
+//const timer = new Timer()
+
+//animation
+const tick =  () => {
+
+    // timer.update()
+
+    // //Variable constante para uniformar el tiempo de animación
+    // const elapsedTime = timer.getElapsed()
+    
+    // mesh.position.set(Math.sin(elapsedTime), Math.cos(elapsedTime), 0)
+    // mesh.lookAt(camera.position)
+    // // Esto es en segundos no milisegundos. 
+    
+    
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+
+    
+}
+
+tick()
